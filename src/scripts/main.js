ee8986d3000582980bf19ff2cb7ca681b54650b6
Marc Camprecios <marc.camprecios@gmail.com>
2022-02-17 19:37:27 +0100

    trying it with captions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,9 +21,35 @@ function autorun(){
   }
 
   // until browsers add support for :has in css...
+  // document.querySelectorAll('.caption').forEach(
+  //   span => span.parentElement.classList.add('captioned')
+  // );
+
+  // Captions are wrapped in a <span> element,
+  // that have a minimum width styles with CSS.
+  // The picture is set to block, and the 
+  // common parent <p> is set to inline-block.
+  // The <p> then gets the pictures' CSS width. 
   document.querySelectorAll('.caption').forEach(
-    span => span.parentElement.classList.add('captioned')
+    span => {
+      let parentWidth = span.parentElement.offsetWidth;
+      span.parentElement.classList.add('captioned');
+
+      //////// Not Working, async or whatever
+      // console.log(parentWidth);
+      // console.log(span);
+      // span.style.width = parentWidth +'px';
+      // span.setAttribute('style','width:'+parentWidth);
+
+      /////// Needs an extra div, so it's better to just create a proper element
+      let newDiv = document.createElement('div');
+      newDiv.classList.add('caption');
+      newDiv.innerHTML = span.innerHTML;
+      span.parentElement.replaceChild(newDiv, span);
+    }
   );
+
+
 };
 
 if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
