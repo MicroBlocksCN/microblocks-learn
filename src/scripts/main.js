e7c6ba36acff8fc61b95e8cdb48ac9bc7d1f987f
Marc Camprecios <marc.camprecios@gmail.com>
2022-02-18 20:05:43 +0100

    forgotten cat meme placeholder.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,7 @@
 function autorun(){
 
   console.log('Website is ready!');
-
   const body = document.getElementsByTagName('body')[0];
-
-  /* if (window.innerWidth > 767) {
-    console.log("JS - Window > 767px");
-  } else {
-    console.log("JS - Window < 767px");
-  } */
 
   menuToggle();
 
@@ -24,32 +17,6 @@ function autorun(){
   document.querySelectorAll('.caption').forEach(
     span => span.parentElement.classList.add('captioned')
   );
-
-  // Captions are wrapped in a <span> element,
-  // that have a minimum width styles with CSS.
-  // The picture is set to block, and the 
-  // common parent <p> is set to inline-block.
-  // The <p> then gets the pictures' CSS width. 
-    /*
-  document.querySelectorAll('.caption').forEach(
-    span => {
-      let parentWidth = span.parentElement.offsetWidth;
-      span.parentElement.classList.add('captioned');
-
-      //////// Not Working, async or whatever
-      // console.log(parentWidth);
-      // console.log(span);
-      // span.style.width = parentWidth +'px';
-      // span.setAttribute('style','width:'+parentWidth);
-
-      /////// Needs an extra div, so it's better to just create a proper element
-      let newDiv = document.createElement('div');
-      newDiv.classList.add('caption');
-      newDiv.innerHTML = span.innerHTML;
-      span.parentElement.replaceChild(newDiv, span);
-    }
-  );*/
-
 
 };
 
