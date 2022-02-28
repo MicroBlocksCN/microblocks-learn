30fd929281e11a2e7a5df83d1a4437057fec7697
Marc Camprecios <marc.camprecios@gmail.com>
2022-02-28 12:56:03 +0100

    mouseover triggers

--- src/scripts/home-hero.js
+++ src/scripts/home-hero.js
@@ -1,51 +1,39 @@
 function homeHero () {
-    console.log('yay at home');
 
-    const elements = [
+    const letters = [
         letterL = document.querySelector('svg #letter-l'),
         letterE = document.querySelector('svg #letter-e'),
         letterA = document.querySelector('svg #letter-a'),
         letterR = document.querySelector('svg #letter-r'),
-        letterN = document.querySelector('svg #letter-n'),
-        microblocks = document.querySelector('svg #microblocks')
+        letterN = document.querySelector('svg #letter-n')
     ]
 
-    console.log(elements);
+    console.log(letters);
 
-    elements.forEach( (element) => {
-        console.log(element);
-        element.addEventListener('click', letterAction(element));
+    letters.forEach( (letter) => {
+        console.log(letter);
+        letter.addEventListener('mouseover', function(e) {
+            letterAction(e);
+        });
+        letter.addEventListener('mouseout', letterReset);
     });
 
 
-
-    function letterAction(letter) {
-        console.log('accessing letterAction from: ' + letter);
-
-        elements.forEach( (element) => {
-            if (!element==letter) {
-                element.style.opacity = '0.1';
+    function letterAction(event) {
+        let theLetter = event.target;
+        
+        letters.forEach( (letter) => {
+            if (letter != theLetter) {
+                // console.log(letter);
+                letter.style.opacity = '0.1';
             }
         });
     }
-    
-    
-    // const letterL = document.querySelector('svg #letter-l');
-    // const letterE = document.querySelector('svg #letter-e');
-    // const letterA = document.querySelector('svg #letter-a');
-    // const letterR = document.querySelector('svg #letter-r');
-    // const letterN = document.querySelector('svg #letter-n');
-    // const microblocks = document.querySelector('svg #microblocks');
 
 
-    // console.log(letterN);
-    // letterN.style.opacity = '0.1';
-
-    // document.addEventListener('mouseover', hoverLetters);
-
-    // function hoverLetters(e) {
-    //     console.log(e);
-    //     let letter = e.target.id;
-    //     console.log(letter);
-    // }
+    function letterReset() {
+        letters.forEach( (letter) => {
+            letter.style.opacity = '1';
+        });
+    }
 };
