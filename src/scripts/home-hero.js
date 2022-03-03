b44da93826a4e7694750f6cf20408a7b7e6a692d
Marc Camprecios <marc.camprecios@gmail.com>
2022-03-03 18:24:47 +0100

    animating letters

--- src/scripts/home-hero.js
+++ src/scripts/home-hero.js
@@ -1,6 +1,7 @@
 function homeHero () {
 
     
+    const microblocksText = document.querySelector('svg #MicroBlocks');
     const letters = [
         L = document.querySelector('svg #letter-l'),
         E = document.querySelector('svg #letter-e'),
@@ -11,59 +12,38 @@ function homeHero () {
 
     const data = {
         L: {
-            text: document.querySelector('.c_hero-home__text-l'),
-            bubble: document.querySelector('.c_hero-home__bubble-l'),
+            text: document.querySelector('.c_hero-home__desc--l'),
+            bubble: document.querySelector('.c_hero-home__bubble--l'),
             url: "https://en.wikipedia.org/wiki/Adele_Goldberg_(computer_scientist)"
         },
         E: {
-            text: document.querySelector('.c_hero-home__text-e'),
-            bubble: document.querySelector('.c_hero-home__bubble-e'),
+            text: document.querySelector('.c_hero-home__desc--e'),
+            bubble: document.querySelector('.c_hero-home__bubble--e'),
             url: "https://en.wikipedia.org/wiki/Adele_Goldberg_(computer_scientist)"
         },
         A: {
-            text: document.querySelector('.c_hero-home__text-a'),
-            bubble: document.querySelector('.c_hero-home__bubble-a'),
+            text: document.querySelector('.c_hero-home__desc--a'),
+            bubble: document.querySelector('.c_hero-home__bubble--a'),
             url: "https://en.wikipedia.org/wiki/Adele_Goldberg_(computer_scientist)"
         },
         R: {
-            text: document.querySelector('.c_hero-home__text-r'),
-            bubble: document.querySelector('.c_hero-home__bubble-r'),
+            text: document.querySelector('.c_hero-home__desc--r'),
+            bubble: document.querySelector('.c_hero-home__bubble--r'),
             url: "https://en.wikipedia.org/wiki/Adele_Goldberg_(computer_scientist)"
         },
         N: {
-            text: document.querySelector('.c_hero-home__text-n'),
-            bubble: document.querySelector('.c_hero-home__bubble-n'),
+            text: document.querySelector('.c_hero-home__desc--n'),
+            bubble: document.querySelector('.c_hero-home__bubble--n'),
             url: "https://en.wikipedia.org/wiki/Adele_Goldberg_(computer_scientist)"
         }
     }
-
-    // console.log(data);
-    // console.log(data.A);
-    // console.log(data.A.letter);
-
-    // Object.entries(data).forEach( ([key, value]) => {
-    //     let letter = value.letter;
-    //     let text = value.text;
-    //     let url = value.url;
-    //     // console.log(letter);
-    //     // console.log(text);
-    //     // console.log(url);
-        
-    //     // letter.addEventListener('mouseover', function(event) {
-    //     //     let localLetter = letter;
-    //     //     // console.log(localLetter);
-    //     //     letterAction(event, localLetter);
-    //     // });
-    //     // letter.addEventListener('mouseout', letterReset);
-
-    // });
 
 
     // console.log(letters);
 
     letters.forEach( (letter) => {
         let letterName = letter.dataset.letter;
-        
+                
         // console.log(letter);
         letter.addEventListener('mouseover', function(e) {
             letterAction(e);
@@ -71,7 +51,8 @@ function homeHero () {
 
         letter.addEventListener('click', function(e) {
             let letterURL = data[letterName].url;
-            console.log(letterURL);
+            window.open(letterURL, 'tab');
+
         });
 
         letter.addEventListener('mouseout', function(e) {
@@ -81,34 +62,51 @@ function homeHero () {
 
 
     function letterAction(event) {
-        let theLetter = event.target;
+
+        // find the letter
+        // target points to the rectangle area,
+        // so we go for the parent element
+        let theLetter = event.target.parentElement;
+
+        // find the elements
         let letterName = theLetter.dataset.letter;
         let letterBubble = data[letterName].bubble;
         let letterText = data[letterName].text;
 
-        letterBubble.style.display = 'block';
-        letterText.style.display = 'block';
+        // change states
+        letterBubble.classList.add('c_hero-home__bubble--is-visible');
+        letterText.classList.add('c_hero-home__desc--is-visible');
+        microblocksText.classList.add('c_home-hero__microblocks-text--hidden');
         
+        // letter states
         letters.forEach( (letter) => {
             if (letter != theLetter) {
-                // console.log(letter);
-                letter.style.opacity = '0.1';
+                letter.classList.add('c_home-hero__letter--hidden');
             }
         });
     }
 
 
     function letterReset(event) {
-        let theLetter = event.target;
+
+        // find the letter
+        // target points to the rectangle area,
+        // so we go for the parent element
+        let theLetter = event.target.parentElement;
+
+        // find the elements
         let letterName = theLetter.dataset.letter;
         let letterBubble = data[letterName].bubble;
         let letterText = data[letterName].text;
 
-        letters.forEach( (letter) => {
-            letter.style.opacity = '1';
-        });
+        // change states
+        letterBubble.classList.remove('c_hero-home__bubble--is-visible');
+        letterText.classList.remove('c_hero-home__desc--is-visible');
+        microblocksText.classList.remove('c_home-hero__microblocks-text--hidden');
 
-        letterBubble.style.display = 'none';
-        letterText.style.display = 'none';
+        // letter states
+        letters.forEach( (letter) => {
+            letter.classList.remove('c_home-hero__letter--hidden');
+        });
     }
 };
